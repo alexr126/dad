46490b5203751875f5eb37bda3e5f4b4fcd20843
FilipaFonte <f.fonte18@gmail.com>
2017-11-23 23:18:04 +0000

    Alteração do código dos Users para trabalhar com componentes Vue (ainda não funciona)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,38 +1,31 @@
 @extends('master')
 
-@section('metatags')
-    <meta name="csrf-token" content="{{ csrf_field() }}">
-@endsection
-
-@section('title', 'List users')
+@section('title', 'Vue.js App')
 
 @section('content')
 
-    <br>
+<div class="jumbotron">
+    <h1>@{{ title }}</h1>
+</div>
 
-    <table id="datatable" class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nickname</th>
-                <th>Name</th>
-                <th>Email</th>                
-                <th>Admin</th>
-                <th>Blocked</th>
-                <th>Reason Blocked</th>
-                <th>Reason Reactivated</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-            </tr>
-        </thead>
-    </table>
+<users :users="users" @edit-click="editUser" @delete-click="deleteUser">
+</users>        
 
-    <div id="navigationBar"></div>
+<div class="alert alert-success" v-if="showSuccess">
+    <button type="button" class="close-btn" v-on:click="showSuccess=false">&times;</button>
+    <strong>@{{ successMessage }}</strong>
+</div>
+
+<user-edit>
+</user-edit>
 
 @endsection
-
 @section('pagescript')
-    <script src="/js/usersDatatable.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-@endsection
+<script src="js/vueapp.js"></script>
+
+<!-- <script src="/js/manifest.js"></script>
+<script src="/js/vendor.js"></script>
+<script src="/js/vueapp.js"></script>
+ -->
+ @stop  
+
