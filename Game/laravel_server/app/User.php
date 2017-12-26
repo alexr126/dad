d78a1596e7e8fbca2809a73dddf41363121538d7
FilipaFonte <f.fonte18@gmail.com>
2017-12-26 14:31:05 +0000

    Criação das pastas para os dois projetos

---
+++ Game/laravel_server/app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email', 
+        'nickname', 
+        'admin', 
+        'blocked', 
+        'reason_blocked', 
+        'reason_reactivated'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password'
+    ];
+
+    public function games()
+    {
+        return $this->belongsToMany('App\Game');
+    }
+}
